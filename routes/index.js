f7d6bf4b7447f7e887b97c4df78dbfd2ddefddc9
Angela Donati <angeladonati93@gmail.com>
2022-03-08 12:53:04 -0500

    remove routes, condense into server.js for simplicity

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-// import routes for notes
-const notesRouter = require('./notes');
-const app = express();
-
-app.use('/notes', notesRouter);
-
-module.exports = app;
--- server.js
+++ server.js
@@ -1,37 +1,32 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const notes = require('./Develop/db/db.json');
 const api = require('./routes/index');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 
 // middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', api);
+
 // make public folder static
 app.use(express.static('public'));
 
+// html routes
 // GET route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 })
 // GET route for notes page 
-app.get('/notes.html', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 })
-
-// get notes
-// app.get('/api/notes', (req, res) => {
-//     res.json(notes);
-// })
-
-
-// post
-// delete
-
+// listen at port
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)
 });
+
+// API Routes
